d5907d44df18ec6a204c870e47e7e63ee7986a5e
wouterstoter <wouterstoter@users.noreply.github.com>
2020-05-12 15:01:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+    return new Response('The network is totally failed');
   event.respondWith(fetch(event.request).then(function (response) {
     console.log(response);
     return response;
