ae79ca08fa4eaac7654f4292246c065a8e084b93
wouterstoter <wouterstoter@users.noreply.github.com>
2020-05-12 15:25:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function(event) {
   
 });
 
-self.onfetch = function(event) {
+self.onfetch = function(event) {console.log(event.request);
     event.respondWith(
     new Response('<p>This is a response that comes from your service worker!</p>', {
        headers: {'Content-Type': 'text/html'}
