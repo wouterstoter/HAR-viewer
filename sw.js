5818b6937a8934ca4fcb30105ffcf1bbd426cfb5
wouterstoter <wouterstoter@users.noreply.github.com>
2020-05-12 15:34:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,11 +10,18 @@ self.addEventListener('install', function(event) {
 });
 
 self.onfetch = function(event) {console.log(event.request);
-    event.respondWith(
-    new Response('<p>This is a response that comes from your service worker!</p>', {
-       headers: {'Content-Type': 'text/html'}
-      })
-    );
+    if (dont.indexOf(event.request.url) != -1) {
+        event.respondWith(fetch(event.request).then(function (response) {
+            console.log(response);
+            return response;
+        }))
+    } else {
+        event.respondWith(
+            new Response('<p>This is a response that comes from your service worker!</p>', {
+                headers: {'Content-Type': 'text/html'}
+            })
+        );
+    }
   /*event.respondWith(fetch(event.request).then(function (response) {
     console.log(response);
     return response;
