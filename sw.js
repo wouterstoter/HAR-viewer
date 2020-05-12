f17bf875ef3e399179900637ae7b77a343a0ff1f
wouterstoter <wouterstoter@users.noreply.github.com>
2020-05-12 14:08:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 self.addEventListener('install', function(event) {
-  console.log("sw installed")
+  
 });
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     console.log(event.request);
-    return fetch(event.request);
+    return fetch(event.request).then(function (response) {
+      return response;
+    });
   );
 });
